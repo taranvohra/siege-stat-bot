993693da6e8e35266fa47121cb096b4ae8bb1da1
taranvohra <taran.vohra@hotmail.com>
2020-12-13 01:44:14 +0530

    finalizing changes

--- src/fonts.js
+++ src/fonts.js
@@ -1,26 +1,26 @@
 const Jimp = require('jimp');
 
 const FONTS = (async () => {
-  const redMainFNT = await Jimp.loadFont('../assets/main_red.fnt');
-  const blueMainFNT = await Jimp.loadFont('../assets/main_blue.fnt');
-  const redMainLgFNT = await Jimp.loadFont('../assets/main_red-lg.fnt');
-  const blueMainLgFNT = await Jimp.loadFont('../assets/main_blue-lg.fnt');
-  const langarNukesFNT = await Jimp.loadFont('../assets/langar-nukes.fnt');
+  const redMainFNT = await Jimp.loadFont('assets/main_red.fnt');
+  const blueMainFNT = await Jimp.loadFont('assets/main_blue.fnt');
+  const redMainLgFNT = await Jimp.loadFont('assets/main_red-lg.fnt');
+  const blueMainLgFNT = await Jimp.loadFont('assets/main_blue-lg.fnt');
+  const langarNukesFNT = await Jimp.loadFont('assets/langar-nukes.fnt');
   const langarNukesFailedFNT = await Jimp.loadFont(
-    '../assets/langar-nukes-failed.fnt'
+    'assets/langar-nukes-failed.fnt'
   );
   const langarNukesKilledFNT = await Jimp.loadFont(
-    '../assets/langar-nukes-killed.fnt'
+    'assets/langar-nukes-killed.fnt'
   );
   const langarMineFragsFNT = await Jimp.loadFont(
-    '../assets/langar-mine-frags.fnt'
+    'assets/langar-mine-frags.fnt'
   );
-  const langarBuildsFNT = await Jimp.loadFont('../assets/langar-builds.fnt');
-  const langarEffFNT = await Jimp.loadFont('../assets/langar-eff.fnt');
-  const langarDeathsFNT = await Jimp.loadFont('../assets/langar-deaths.fnt');
-  const fragsRedFNT = await Jimp.loadFont('../assets/frags-red.fnt');
-  const fragsBlueFNT = await Jimp.loadFont('../assets/frags-blue.fnt');
-  const langarOrangeFNT = await Jimp.loadFont('../assets/langar-orange.fnt');
+  const langarBuildsFNT = await Jimp.loadFont('assets/langar-builds.fnt');
+  const langarEffFNT = await Jimp.loadFont('assets/langar-eff.fnt');
+  const langarDeathsFNT = await Jimp.loadFont('assets/langar-deaths.fnt');
+  const fragsRedFNT = await Jimp.loadFont('assets/frags-red.fnt');
+  const fragsBlueFNT = await Jimp.loadFont('assets/frags-blue.fnt');
+  const langarOrangeFNT = await Jimp.loadFont('assets/langar-orange.fnt');
 
   return {
     redMainFNT,
